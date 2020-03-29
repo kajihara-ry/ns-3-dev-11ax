0efd31c6c41d7af97ad46d28459b0a977f4ea2cd
kajhara-ry <kajihara.ry@dsp.cse.kyutech.ac.jp>
2020-03-29 13:58:17 +0900

    backoffcouunt

--- src/wifi-he/scripts/backoff_count_test.sh
+++ src/wifi-he/scripts/backoff_count_test.sh
@@ -3,7 +3,7 @@ source spatial-reuse-functions.sh
 cd ../examples
 export AUTO_DELETE_SPATIAL_REUSE_OUTPUT_FILES=1
 
-export enablePcap=0
+export enablePcap=1
 export enableAscii=0
 export rxSensitivity=-82
 export standard=11ac
@@ -12,7 +12,7 @@ obssPdLevel=-82
 #export NS_LOG=DynamicObssPdAlgorithm=logic
 
 for i in 1; do #Random Number Seed
-for j in 20; do #STAs per BSS
+for j in 1; do #STAs per BSS
 RngRunpar=${i}
 npar=${j}
 Res=Sim0
@@ -23,7 +23,7 @@ export RngRun=${RngRunpar} #Random Number Seed
 export obssPdAlgorithm=ConstantObssPdAlgorithm
 export obssPdThreshold=-82 # only used by 11ax
 export maxAmpduSize=65535 #MAX AMPDU Size [octet] df 2000
-export MCS=7 #MCS Setting
+export MCS=0 #MCS Setting
 export downlink=0
 export uplink=400 #Uplink Offered Load [Mbps]
 export duration=5 #Simulation Duration [s]
@@ -39,8 +39,8 @@ export performTgaxTimingChecks=0
 export nodePositionsFile=NONE
 export bw=20 #Bandwidth [Mhz]
 export scenario=logdistance #Pass Loss Model
-export nBss=1 #Number of BSSs
-export payloadSizeUplink=1472 #Uplink Payload Size [octet] df 1500
+export nBss=2 #Number of BSSs
+export payloadSizeUplink=1472 #Uplink Payload Size(MSDU) [octet] df 1500
 export payloadSizeDownlink=1500
 export useIdealWifiManager=0
 export r=0.1 #BSS Range[m]
@@ -49,7 +49,7 @@ export powAp=20
 export txRange=102
 export ccaTrSta=-62 #CSR_Threshold(STA) [dBm] -62=>8.2[m] -82=>30.5[m]
 export ccaTrAp=-62 #CSR_Threshold(AP) [dBm]
-export d=10 #BSS distance [m]
+export d=7 #BSS distance [m]
 export sigma=3.5
 export bianchi=1
 export n=${npar} #STAs per BSS
